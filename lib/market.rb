6062669e5cdf0c92352a4a0bc58e1dcf42ac3f68
sbstn-jmnz <sbstn.jmnz@yahoo.com>
2020-01-04 16:23:28 -0300

    Basic Market class described

---
+++ lib/market.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class Market
+    
+  def initialize()
+  end
+
+  def orders
+    []
+  end
+
+  def transactions
+    []
+  end
+end
+
+class Order
+end
+
+class Transaction
+  
+end
--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -1,27 +1,27 @@
+# frozen_string_literal: true
+
 require 'market'
 
 describe 'Market' do
+ 
   describe 'attributes' do
+    before(:example) do
+      @market = Market.new
+    end
+
     it 'has an array of Order instances in its :orders atributes' do
-      market = Market.new
-      market.orders << Order.new
-      expect(market.orders).to be_an_instance_of(Array)
+      @market = Market.new
+      @market.orders << Order.new
+      expect(@market.orders).to be_an_instance_of(Array)
+    end
+
+    it 'has an array of Transaction instances in its @transactions attributes' do
+      @market = Market.new
+      @market.transactions << Transaction.new
+      expect(@market.transactions).to be_an_instance_of(Array)
     end
   end
 
-  describe "#orders" do
-    
+  describe '#to_s' do
   end
-
-  describe "#transactions" do
-    
-  end
-
-  describe "#to_s" do
-    
-  end
-  
-  
-  
-
 end
